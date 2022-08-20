805bc2120affef2b610655dabffe8f23f13d351b
whitefox <fmjj008@gmail.com>
2022-08-20 16:08:52 +0900

    init

---
+++ src/main/resources/readme.sql
@@ -0,0 +1,18 @@
+1. 홈페이지 템플릿(Simple Homepage)
+로그인계정(관리자) : admin 로그인암호 : 1
+
+2. 표준프레임워크 샘플포탈(Portal Site)
+로그인계정(관리자) : admin , 로그인암호 : 1
+로그인계정(사용자) : user1 , 로그인암호 : 1
+
+3. 샘플 내부 업무사이트(Enterprise Business)
+로그인계정(관리자) : admin , 로그인암호 : 1
+로그인계정(사용자) : user1 , 로그인암호 : 1
+
+4. 공통컴포넌트 (Common All-in-one)
+* 대소문자 유의
+
+구분 : 일반사용자, ID : USER, PW : rhdxhd12    (영문으로 공통12)
+구분 : 기업사용자, ID : ENTERPRISE, PW : rhdxhd12 (영문으로 공통12)
+구분 : 업무사용자, ID : TEST1, PW : rhdxhd12 (영문으로 공통12)
+구분 : 업무사용자, ID : webmaster, PW : rhdxhd12 (영문으로 공통12)
